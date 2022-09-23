b95b43ae67133e0d4e0c6e5ad58c1c61736ab81b
SirmaXX <sea-97@hotmail.com>
2022-09-23 19:40:46 +0300

    arayüz flask'a uyarlandı

--- Webapp/app.py
+++ Webapp/app.py
@@ -1,9 +1,24 @@
-from flask import Flask, request,json
+from flask import(Flask,jsonify,make_response,request,render_template,abort,make_response)
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello!'
+def index():
+        return render_template("index.html" )
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html" )
+
+
+@app.route('/login')
+def login():
+     return render_template("login.html" )
+
+
+@app.route('/register')
+def register():
+    return render_template("register.html" )
 
 
 if __name__ == "__main__":
